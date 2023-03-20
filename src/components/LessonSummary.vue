dc1cac2255c85b966ef3a42f75d8093defd2fa6e
Letícia Vieira <lvieira268@gmail.com>
2023-03-20 19:44:42 -0300

    feat: creates LessonSummary component
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

---
+++ src/components/LessonSummary.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+	defineProps({
+		courseId: {
+			type: String,
+			required: true,
+		},
+		lesson: {
+			type: Object,
+			required: true,
+		},
+		num: {
+			type: Number,
+			required: true,
+		}
+	})
+</script>
+
+<template>
+	<section class="summary">
+		<div>
+			<div class="title">
+				<h2>
+					<router-link class="no-underline cursor-pointer" :to="'/courses/' + courseId + '/lessons/' + lesson.id">
+						{{ num }}. {{ lesson.title }}
+					</router-link>
+				</h2>
+			</div>
+			<p>
+				<router-link
+					class="no-underline cursor-pointer"
+					:to="'/course/' + courseId + '/lessons/' + lesson.id"
+				>
+					{{ lesson.description }}
+				</router-link>
+			</p>
+		</div>
+	</section>
+</template>
