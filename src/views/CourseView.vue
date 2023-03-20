e4add14dfdc94f4d6dedd9ad57c3f17866d56eca
Letícia Vieira <lvieira268@gmail.com>
2023-03-20 19:45:06 -0300

    feat: implements LessonSummary component to CourseView
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

--- src/views/CourseView.vue
+++ src/views/CourseView.vue
@@ -1,22 +1,38 @@
 <script lang="ts" setup>
-import { useRoute } from 'vue-router';
-import courses from '../courses';
+  import courses from '../courses';
+  import { useRoute } from 'vue-router';
 
-const route = useRoute()
-const courseId = route.params.courseId
-const course = courses.find(course => course.id === parseInt(courseId as string));
+  import LessonSummary from '../components/LessonSummary.vue';
 
+  const route = useRoute();
+  const courseId = route.params.courseId;
+  const course = courses.find(course => course.id === parseInt(courseId as string));
 </script>
+
 
 <template>
   <div class="Course page">
     <header>
       <p>
-        <router-link :to="{ name: 'home' }">Back to courses</router-link>
+        <router-link :to="{ name: 'home'}">Back to courses</router-link>
       </p>
       <h1>{{ course?.title }}</h1>
       <p>{{ course?.description }}</p>
+      <router-link 
+        class="button primary icon"
+        :to="`/courses/${courseId}/lessons/${course?.lessons[0].id}`"
+      >
+        Start course
+      </router-link>
     </header>
-    
+    <div>
+      <LessonSummary 
+        v-for="(lesson, index) in course?.lessons"
+        :key="index"
+        :course-id="courseId as string"
+        :lesson="lesson"
+        :num="index + 1"
+      />
+    </div>
   </div>
 </template>
